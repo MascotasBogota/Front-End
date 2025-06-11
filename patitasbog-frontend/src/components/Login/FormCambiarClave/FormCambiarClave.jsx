ca1ad6a99ec7f36cfa0c9c25a7a6e304e8c783fa
Juan Ardila <juardilad@unal.edu.co>
2025-06-11 02:56:39 -0500

    Creating ViewCambiarClave and ViewRecuperarClave

--- patitasbog-frontend/src/components/Login/FormCambiarClave/FormCambiarClave.jsx
+++ patitasbog-frontend/src/components/Login/FormCambiarClave/FormCambiarClave.jsx
@@ -1,20 +1,44 @@
 import {React, useState} from 'react'
 import './FormCambiarClave.css'
+import PasswordInput from '../PasswordInput/PasswordInput';
 
 const FormCambiarClave = () => {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [codeConfirmation, setCodeConfirmation] = useState('');
     const [showMessage, setShowMessage] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Nueva Contraseña:', password);
+        console.log('Confirmar Contraseña:', confirmPassword);
+        console.log('Codigo Confirmación:', codeConfirmation);
+    };
+
     return(
         <div className='caja-formulario'>
             <h1>Reestablacer Contraseña</h1>
-            <p>Ingresa el correo electrónico asociado a tu cuenta, 
-                será enviado un código de confirmación que deberás 
-                digitar para hacer efectivo el restablecimiento de 
-                tu contraseña.
-            </p>
-            <span>Correo</span>
-            <form>
-                <input type="email" />
-                <button type="submit">Enviar</button>
+            <p>Ingresa tu nueva contraseña a continuación</p>
+            <form onSubmit={handleSubmit}>
+                <PasswordInput
+                    label="Nueva Contraseña:"
+                    id="newPassword"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="newPassword"
+                    required
+                />
+                <PasswordInput
+                    label="Confirmar Contraseña:"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    name="confirmPassword"
+                    required
+                />
+                <label>Código de confirmación enviado al correo</label>
+                <input type="text" onChange={(e) => setCodeConfirmation(e.target.value)}/>
+                <button type="submit" className="button-principal">Enviar</button>
             </form>
         </div>
     )
--- patitasbog-frontend/src/components/Login/FormRecuperacion/FormRecuperacion.jsx
+++ patitasbog-frontend/src/components/Login/FormRecuperacion/FormRecuperacion.jsx
@@ -3,6 +3,13 @@ import './FormRecuperacion.css'
 
 const FormRecuperacion = () => {
     const [showMessage, setShowMessage] = useState(true);
+    const [correo, setCorreo] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Correo: ', correo);
+    };
+
     return(
         <div className='caja-formulario'>
             <h1>Reestablacer Contraseña</h1>
@@ -11,10 +18,11 @@ const FormRecuperacion = () => {
                 digitar para hacer efectivo el restablecimiento de 
                 tu contraseña.
             </p>
-            <span>Correo</span>
-            <form>
-                <input type="email" />
-                <button type="submit">Enviar</button>
+            <form onSubmit={handleSubmit}>
+                <label>Correo</label>
+                <input type="email" onChange={(e) => setCorreo(e.target.value)}/>
+                <button type="submit" className="button-principal"
+                >Enviar</button>
             </form>
         </div>
     )
