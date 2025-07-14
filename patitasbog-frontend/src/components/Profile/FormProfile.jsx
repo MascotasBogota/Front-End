6eac509af72c93d41b473bff3207d1ccf2dd37b4
Juan Ardila <juardilad@unal.edu.co>
2025-07-14 16:57:28 -0500

    Adding profile view and profile form

---
+++ patitasbog-frontend/src/components/Profile/FormProfile.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from '../../styles/Profile.module.css';
+
+const FormProfile = () => {
+  return (
+    <div className={styles.form_container}>
+        <p className={styles.title}>Mi Perfil</p>
+        <form className={styles.form}>
+            <div className={styles.options_column}>
+                <img src='/images/sin_foto_perfil.png'
+                    className={styles.foto_perfil}
+                ></img>
+                <button type="button" className={styles.options_button}>Subir foto de perfil</button>
+                <button type="submit" className={styles.options_button}>Guardar cambios</button>
+                <Link to="/change_password" className={styles.options_button2}>Cambiar contraseña</Link>
+            </div>
+            <div className={styles.fields_column}>
+                <label className={styles.inputLabel}>Nombre completo</label>
+                <input className={styles.formInput}></input>
+                <label className={styles.inputLabel}>Correo electrónico</label>
+                <input className={styles.formInput}></input>
+                <label className={styles.inputLabel}>Usuario</label>
+                <input className={styles.formInput}></input>
+                <label className={styles.inputLabel}>Sexo</label>
+                <input className={styles.formInput}></input>
+                <label className={styles.inputLabel}>Dirección</label>
+                <input className={styles.formInput}></input>
+                <label className={styles.inputLabel}>Número de teléfono</label>
+                <input className={styles.formInput}></input>
+            </div>
+        </form>
+    </div>
+  );
+};
+
+export default FormProfile;
