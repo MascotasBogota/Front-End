0c59a826d0f5450180ec9bfb686ddd8bad172b07
Juan Ardila <juardilad@unal.edu.co>
2025-07-21 21:40:11 -0500

    No sé que estoy haciendo

---
+++ patitasbog-frontend/src/components/Notificaciones/NotificationWindow.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react"
+import styles from "../../styles/ReportForm.module.css"
+import { imageService } from "../../services/imageService"
+
+const NotificationWindow = ( { data, idviewer, idreport } ) => {
+    
+}
+
+export default NotificationWindow;
--- patitasbog-frontend/src/views/Layout.jsx
+++ patitasbog-frontend/src/views/Layout.jsx
@@ -1,8 +1,28 @@
-import React, { useState, useEffect } from 'react'; // Importamos useState y useEffect
+import React, { useState, useEffect, useContext } from 'react'; // Importamos useState y useEffect
 import Navbar from '../components/Principal/Navbar'; 
 import styles from '../styles/Layout.module.css';
+import { AuthContext } from "../contexts/AuthContext"
+import { notificationService } from '../services/notificationService';
 
 const Layout = ({ children }) => {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    useEffect(() => {
+        const fetchNotificacions = async () => {
+            try {
+                console.log("Notificaciones");
+                const response = await notificationService.getCurrentUserNotifications();
+                console.log(response);
+                setIdUserLogued(response.profile._id);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        if (isAuthenticated) {
+            fetchNotificacions();
+        }
+    }, [isAuthenticated]);
 
     return (
         <div className={styles.view}>
