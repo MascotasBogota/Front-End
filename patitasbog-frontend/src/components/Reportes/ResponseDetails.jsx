19f6404d4b01cfab1ef160ce7af54b67c7f93e9b
J-HuertaS <juan.cardenas.huertas@gmail.com>
2025-07-22 07:45:00 -0500

    si

--- patitasbog-frontend/src/components/Reportes/ResponseDetails.jsx
+++ patitasbog-frontend/src/components/Reportes/ResponseDetails.jsx
@@ -135,7 +135,7 @@ const ResponseDetails = ( { data, idviewer, idreport, report } ) => {
                 </div>
                 <span className={styles.response_type_label}>{type == "avistamiento" ? "Avistamiento" : "Encontrado"}</span>
                 {idviewer == idresponseUser && (
-                    <img src='/icons/pencil.svg' className={styles.response_change_icon} onClick={handleEdit}/>
+                    <img src='/icons/pencil.svg' alt='pencil-response' className={styles.response_change_icon} onClick={handleEdit}/>
                 )
                 }
                 {idviewer == idresponseUser && (
